086b01207d6111197645a62057015ee316b5ba37
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2024-11-04 02:05:47 +0530

    Revision BFS

--- 4_bfs_traversal_technique.cpp
+++ 4_bfs_traversal_technique.cpp
@@ -1,50 +1,80 @@
 #include<iostream>
+#include<map>
 #include<vector>
 #include<queue>
 using namespace std;
 
-int main() {
-    cout << "Enter no. of nodes and edges: ";
-    int n, e; cin >> n >> e;
+void bfsTraversal(map<int, vector<int> > &mp) {
+    int visited[mp.size()+1] = {0};
 
-    vector<int> adjList[n+1];
+    int startNode;
+    cout << "Enter starting node for bfs traversal: ";
+    cin >> startNode;
 
-    // take input of edges(u, v)
-    for(int i = 0; i < e; i++) {
-        int u, v; cin >> u >> v;
-        adjList[u].push_back(v);
-        // adjList[v].push_back(u);     
+    auto it = mp.find(startNode);
+    if(it == mp.end()) {
+        cout << "Starting Node not found" << endl;
+    }
+    else {
+        queue<int> q;
+        q.push(startNode);
+        q.push(-1);
+        visited[startNode] = 1;
+
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            if(node == -1) {
+                if(!q.empty()) q.push(-1);
+                cout << endl;
+            }
+            else {
+                cout << node << " ";
+                for(int child: mp[node]) {
+                    if(visited[child] != 1) {
+                        visited[child] = 1;
+                        q.push(child);
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+int main(){
+    map<int, vector<int> > mp;
+
+    cout << "Enter starting and ending node: (-1 and -1 values for exit)" << endl;
+
+    int start = 0, end = 0;
+    cout << "starting node: ";
+    cin >> start;
+    cout << "Ending node: ";
+    cin >> end;
+
+    while(start != -1) 
+    {
+        mp[start].push_back(end);
+        mp[end].push_back(start);
+
+        cout << "starting node: ";
+        cin >> start;
+        cout << "Ending node: ";
+        cin >> end;
     }
 
-    cout << "displaying adj list:" << endl;
-    for(int i = 0; i < n+1; i++) {
-        cout << i << "-> ";
-        for(int j = 0; j < adjList[i].size(); j++) {
-            cout << adjList[i][j] << " ";
+    cout << "Displaying adjacency list: " << endl;
+    for(auto it: mp) {
+        cout << it.first << " -> ";
+        for(auto val: it.second) {
+            cout << val << ", ";
         }
         cout << endl;
     }
 
-
-    int visited[n+1] = {0};
-
-    queue<int> q;
-    q.push(1);
-    visited[1] = 1;
-
-    cout << "BFS: " << endl;
-    while(!q.empty()) {
-        int node = q.front();
-        q.pop();
-        cout << node << " ";
-
-        for(auto it: adjList[node]) {
-            if(!visited[it]) {
-                q.push(it);
-                visited[it] = 1;
-            }
-        }        
-    }
-
+    bfsTraversal(mp);
+    
     return 0;
 }
